087edcf4a45ee821fab5b7d648ca75d4215533c1
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-06-09 13:55:24 +1200

    update tests and description

--- verification/referee.py
+++ verification/referee.py
@@ -2,8 +2,17 @@ from checkio.signals import ON_CONNECT
 from checkio import api
 from checkio.referees.io import CheckiOReferee
 
+from checkio.referees.cover_codes import unwrap_args
+
 from tests import TESTS
 
 api.add_listener(
     ON_CONNECT,
-    CheckiOReferee(tests=TESTS).on_ready)
+    CheckiOReferee(
+        tests=TESTS,
+        cover_code={
+            'python-27': unwrap_args,  # or None
+            'python-3': unwrap_args
+        },
+        function_name='convert'
+    ).on_ready)
